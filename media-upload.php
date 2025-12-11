2bd2d4681ddac2a7041a7223724feb49dcf88e71
Md. Inzamam-Ul Huq <87912357+dev-inzams@users.noreply.github.com>
2025-12-11 21:59:28 +0600

    Update media-upload.php

--- media-upload.php
+++ media-upload.php
@@ -4,3 +4,7 @@ curl -X POST \
   -H "Content-Type: image/jpeg" \
   --data-binary "@sunset.jpg" \
   "https://example.com/wp-json/wp/v2/media"
+
+
+
+echo -n "technuxt@gmail.com:ouKy rrVK z08U 065b GsJx tb0n" | base64
