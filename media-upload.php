ec5353b75e7c4bede0c837fee433a45de37d003a
Md. Inzamam-Ul Huq <87912357+dev-inzams@users.noreply.github.com>
2025-12-11 19:40:11 +0600

    Update media-upload.php

--- media-upload.php
+++ media-upload.php
@@ -1,5 +1,5 @@
 curl -X POST \
-  -H "Authorization: Basic 64 data" \
+  -H "Authorization: Basic echo -n "technuxt@gmail.com:57pn lT9A MmKN zuGA ScGN LVj9" | base64" \
   -H "Content-Disposition: attachment; filename=\"sunset.jpg\"" \
   -H "Content-Type: image/jpeg" \
   --data-binary "@sunset.jpg" \
